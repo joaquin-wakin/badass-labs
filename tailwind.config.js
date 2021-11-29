3f196955677f2f350f7d1300811df71ad4faa4d5
John Joaquin Valdez <65848689+joaquin-wakin@users.noreply.github.com>
2021-11-29 17:27:03 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'jit',
+  purge: [path.join(__dirname, 'index.html'), path.join(__dirname, 'src/**/*.js')],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      colors: {
+        primary: '#b9db01',
+        neutral: '#8d8d8d',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
