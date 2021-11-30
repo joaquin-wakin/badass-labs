49dd797deb84c65f40014dd724e3564b80c78141
John Joaquin Valdez <65848689+joaquin-wakin@users.noreply.github.com>
2021-11-30 15:33:27 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,15 +14,21 @@ module.exports = {
       colors: {
         primary: '#b9db01',
         neutral: '#8d8d8d',
+        'light-gray': '#d7d7d7',
       },
       fontFamily: {
         sans: ['Gilroy', ...defaultTheme.fontFamily.sans],
         'ardela-edge': ['ArdelaEdgeX02', ...defaultTheme.fontFamily.sans],
       },
       backgroundImage: {
+        'star-solid': "svg-load('star-solid.svg')",
         'about-us': "url('/images/about-bg-image.jpg')",
         footer: "url('/images/footer-bg-image.png'), linear-gradient(0deg, #000, #000)",
         'arrow-icon': "svg-load('arrow-right-icon.svg')",
+      },
+      boxShadow: {
+        'buy-now': '0 0 8px rgba(165, 165, 165, 0.5)',
+        'pricing-card': '0 0 12px rgba(115, 115, 115, 0.5)',
       },
     },
   },
