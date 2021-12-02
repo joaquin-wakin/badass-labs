b31a1c7f5f3e9c192470ca5b2aa69935be9ff159
John Joaquin Valdez <65848689+joaquin-wakin@users.noreply.github.com>
2021-12-02 10:48:48 +0800

    Rename import

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
-import './css/tailwind.css';
+import './css/index.css';
 import './scss/styles.scss';
+/* import Splide from '@splidejs/splide';
+
+const splide = new Splide('.splide', {
+  rewind: true,
+  perPage: 5,
+  gap: '1.125rem',
+  arrows: true,
+});
+
+splide.mount(); */
