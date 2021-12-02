2b43a4ff05da96b3b8df3e3092ca381871a5c0ea
John Joaquin Valdez <65848689+joaquin-wakin@users.noreply.github.com>
2021-12-02 15:24:03 +0800

    Import fonts stylesheet

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
+import './css/fonts.css';
 import './css/index.css';
 import './scss/styles.scss';
-/* import Splide from '@splidejs/splide';
-
-const splide = new Splide('.splide', {
-  rewind: true,
-  perPage: 5,
-  gap: '1.125rem',
-  arrows: true,
-});
-
-splide.mount(); */
