ab37f4438299f69d5576c67dfe8445041ed45f14
John Joaquin Valdez <65848689+joaquin-wakin@users.noreply.github.com>
2021-12-06 09:50:38 +0800

    Create swiper.js

---
+++ src/js/swiper.js
@@ -0,0 +1,25 @@
+import Swiper, { Navigation } from 'swiper';
+
+Swiper.use([Navigation]);
+
+const swiper = new Swiper('.swiper', {
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 4,
+    },
+    768: {
+      slidesPerView: 5,
+    },
+  },
+  slidesPerView: 4,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+swiper.init();
